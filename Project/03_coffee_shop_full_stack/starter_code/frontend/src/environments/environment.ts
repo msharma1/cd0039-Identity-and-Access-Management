a46b1f1feec014ea620e0bb195848adc8d01837b
msharma1 <manish.sharma0201cs@gmail.com>
2025-03-23 19:11:32 +0000

    Updating frontend environments.ts with my own variables

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,13 @@
 /* @TODO replace with your variables
  * ensure all variables on this page match your project
  */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://localhost:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-vaa4tqxczeu26tio', // the auth0 domain prefix (without .us.auth0.com)
+    audience: 'coffee-shop', // the audience set for the auth0 app
+    clientId: '2OdMNbU0tBlmp3TTtD7YQaBt0kKF7cyf', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
